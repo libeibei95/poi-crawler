75a94f38d479bd84419eb43bc8c2db42ddbf26e7
libeibei <573328344@qq.com>
2018-06-05 11:04:21 +0800

    add

--- ip-picker.js
+++ ip-picker.js
@@ -6,35 +6,12 @@ const url = 'http://www.xicidaili.com/nn/1'
 Rx.Observable.fromPromise(request.get(url))
   .subscribe(res=>{
     var $=cheerio.load(res)
-    console.log("-------------")
     let ips =[];
+    let tbody = $("#ip_list tbody").children()
 
-    var eles = $("#ip_list tbody").children().slice(1,3).children()
-    console.log(`$("#ip_list tbody").children().slice(1,2) is ${$("#ip_list tbody").children().slice(1,2)}`)
-    console.log('------------------')
-    console.log(`$("#ip_list tbody").children().slice(1,3) is ${$("#ip_list tbody").children().slice(1,3)}`)
-    console.log('------------------')
-    console.log(`eles[1] is ${eles[1]}`)
-    console.log(`length of eles is ${eles.length}`)
-    console.log("-----------------")
-    console.log(eles[1].children[0].data)
-    console.log(eles[2].children[0].data)
-
-    // console.log(Array.from($("#ip_list tbody").children().slice(1)).length)
-
-    // Array.from($("#ip_list tbody").children().slice(1)).map(ele=>{
-    //   // console.log(ele)
-    //   var eles = $(this).children()
-    //   console.log(eles.length)
-    //   // console.log(eles[1].children[0].data)
-    //   // console.log(eles[2].children[0].data)      
-    //   // console.log($(this).children().length)
-    // })
-
-    // $("#ip_list tr").slice(1).map(ele=>{
-    //   let children = $(this).children()
-    //   console.log(children.length)
-    // })   
-
-    // console.log($("#ip_list tr").length)
+    for(let i=1;i<tbody.length;i++){
+      let eles = tbody.slice(i,i+1).children()
+      ips.push(`http://${eles[1].children[0].data}/${eles[2].children[0].data}`)
+    }
+    console.log(ips)
   })
--- proxy-demo.js
+++ proxy-demo.js
@@ -1,17 +1,37 @@
-const rp = require('request-promise')
-const cheerio =require('cheerio')
+const request = require('request-promise')
+const cheerio = require('cheerio')
+const Rx = require('rxjs/Rx');
 const proxyUrl = 'http://195.146.1.90:3128'
-const proxiedRp = rp.defaults({proxy:proxyUrl})
+// const proxiedRp = rp.defaults({ proxy: proxyUrl })
 
-async function main(){
-  const urls = [
-    //"http://www.baidu.com",
-    "http://ip.mafengshe.com/",
-  ]
-  await Promise.all(urls.map(i => get(i)))
+const ip_url = 'http://www.xicidaili.com/nn/1'
+
+const load_url = "http://www.poi86.com/poi/amap/city/430100.html"
+
+async function main() {
+  let ips = await getIps()
+  console.log(ips)
+  // const urls = [
+  //   "http://ip.mafengshe.com/",
+  // ]
+  // await Promise.all(urls.map(i => get(i)))
 }
 
-async function get(url){
+async function getIps() {
+  return Rx.Observable.fromPromise(request.get(ip_url))
+    .subscribe(res => {
+      var $ = cheerio.load(res)
+      let tbody = $("#ip_list tbody").children()
+      let ips=[]
+      for (let i = 1; i < tbody.length; i++) {
+        let eles = tbody.slice(i, i + 1).children()
+        ips.push(`http://${eles[1].children[0].data}/${eles[2].children[0].data}`)
+      }
+      return ips
+    })
+}
+
+async function get(url) {
   const res = await proxiedRp.get(url)
   const $ = cheerio.load(res)
   console.log(res)
